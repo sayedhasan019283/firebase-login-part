78c8740e0f8b95beb1d9db3044697d9f8bc16c11
sayed <sayedhasan019283@gmail.com>
2022-04-10 17:03:57 +0600

    adding code

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h4>this page for log in</h4>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/firebase.inti.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA0cpXLj3C7KLjhowQVKz-KeZPtyUfEbZM",
+    authDomain: "fir-route-integration-6cfee.firebaseapp.com",
+    projectId: "fir-route-integration-6cfee",
+    storageBucket: "fir-route-integration-6cfee.appspot.com",
+    messagingSenderId: "110173624480",
+    appId: "1:110173624480:web:fd369852976176caea51c5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
